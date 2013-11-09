e67e2036aa2fa49be463e5adc08f66c757021d17
elParaguayo <antodudgeon@gmail.com>
2013-11-09 19:04:01 +0000

    Some new methods added

--- simplexbmcjson.py
+++ simplexbmcjson.py
@@ -3,9 +3,56 @@ import urllib2
 import urllib
 
 class SimpleXBMCJSON():
-"""XBMCJSON class"""
+    """XBMCJSON class"""
+    
+    class sort:
+        """Quasi-enum class for sorting results."""
+        NONE  = "none"
+        LABEL  = "label"
+        DATE  = "date"
+        SIZE  = "size"
+        FILE  = "file"
+        PATH  = "path"
+        DRIVETYPE  = "drivetype"
+        TITLE  = "title"
+        TRACK  = "track"
+        TIME  = "time"
+        ARTIST  = "artist"
+        ALBUM  = "album"
+        ALBUMTYPE  = "albumtype"
+        GENRE  = "genre"
+        COUNTRY  = "country"
+        YEAR  = "year"
+        RATING  = "rating"
+        VOTES  = "votes"
+        TOP250  = "top250"
+        PROGRAMCOUNT  = "programcount"
+        PLAYLIST  = "playlist"
+        EPISODE  = "episode"
+        SEASON  = "season"
+        TOTALEPISODES  = "totalepisodes"
+        WATCHEDEPISODES  = "watchedepisodes"
+        TVSHOWSTATUS  = "tvshowstatus"
+        TVSHOWTITLE  = "tvshowtitle"
+        SORTTITLE  = "sorttitle"
+        PRODUCTIONCODE  = "productioncode"
+        MPAA  = "mpaa"
+        STUDIO  = "studio"
+        DATEADDED  = "dateadded"
+        LASTPLAYED  = "lastplayed"
+        PLAYCOUNT  = "playcount"
+        LISTENERS  = "listeners"
+        BITRATE  = "bitrate"
+        RANDOM = "random"
+
+    class sortorder:
+        """Quasi-enum class for implementing sort order."""
+        ASCENDING = "ascending"
+        DESCENDING = "descending"
     
     # Define some constants
+    # TODO - turn these into quasi-enum classes to allow user to 
+    # choose properties
     ARTIST_PROPERTIES = [ "instrument",
                           "style",
                           "mood",
@@ -26,12 +73,16 @@ class SimpleXBMCJSON():
                         "year",
                         "thumbnail",
                         "genre"] 
+ 
+    MOVIE_PROPERTIES = ["thumbnail"] 
     
     # XBMC libraries
     AUDIO = 0
     VIDEO = 1
     PICTURES = 2
-
+    
+    # Autovivification class to simplify creation of parameter dicts
+    # Thanks to Google and StackOverflow for this one!
     class AutoVivification(dict):
         """Implementation of perl's autovivification feature."""
         def __getitem__(self, item):
@@ -40,7 +91,10 @@ class SimpleXBMCJSON():
             except KeyError:
                 value = self[item] = type(self)()
                 return value
-    
+
+########################################################################
+# Methods to initiates class and send requests                         #
+########################################################################    
     def __init__(self, server="http://localhost:8080/jsonrpc"):
         """Initialise the class.
         
@@ -81,7 +135,10 @@ class SimpleXBMCJSON():
             return response["result"]
         except:
             return None
-        
+
+########################################################################
+# XBMC input control methods                                           #
+########################################################################
     def Left(self):
         return self.__sendRequest("Input.Left")
         
@@ -99,7 +156,10 @@ class SimpleXBMCJSON():
         
     def Back(self):
         return self.__sendRequest("Input.Back")
-        
+
+########################################################################
+# GUI methods                                                          #
+########################################################################        
     def ShowNotification(self, title, message):
         """Shows pop-up notification.
         
@@ -109,7 +169,10 @@ class SimpleXBMCJSON():
         params["title"] = title
         params["message"] = message
         return self.__sendRequest("GUI.ShowNotification",params)
-        
+
+########################################################################
+# Player methods                                                       #
+########################################################################        
     def PlayerOpenFile(self, item):
         """Opens file.
         
@@ -118,9 +181,31 @@ class SimpleXBMCJSON():
         params = self.AutoVivification()
         params["item"]["file"] = item
         return self.__sendRequest("Player.Open",params)
+
+    def PlayerPlayMovie(self, movieid):
+        """Opens file.
+        
+        Takes local path or URL.
+        """
+        params = self.AutoVivification()
+        params["item"]["movieid"] = movieid
+        return self.__sendRequest("Player.Open",params)
         
     def GetActivePlayers(self):
         return self.__sendRequest("Player.GetActivePlayers")
+
+    def PlayerPlayAlbum(self, albumid):
+        """Opens file.
+        
+        Takes local path or URL.
+        """
+        params = self.AutoVivification()
+        params["item"]["albumid"] = albumid
+        return self.__sendRequest("Player.Open",params)
+
+########################################################################
+# Library methods                                                      #
+########################################################################
         
     def ScanLibrary(self, library):        
         """Updates library. 
@@ -133,6 +218,8 @@ class SimpleXBMCJSON():
             return self.__sendRequest("VideoLibrary.Scan")
         else:
             raise Exception("Can't scan library. Invalid library.")
+
+#### AUDIO LIBRARY #####################################################
             
     def AudioGetArtists(self):
         return self.__sendRequest("AudioLibrary.GetArtists")["artists"]
@@ -150,13 +237,39 @@ class SimpleXBMCJSON():
         params["properties"] = self.ALBUM_PROPERTIES
         return self.__sendRequest("AudioLibrary.GetAlbums",params)
 
+#### VIDEO LIBRARY #####################################################
+
+    def GetMovies(self, start = 0, num = 0 , sort = sort.TITLE, 
+                  sortorder = sortorder.ASCENDING, 
+                  ignorearticle = True):
+       
+        params = self.AutoVivification()
+        params["properties"] = self.MOVIE_PROPERTIES
+        params["limits"]["start"] = start
+        if num > 0:
+            params["limits"]["end"] = start + num
+        params["sort"]["method"] = sort
+        params["sort"]["order"] = sortorder
+        params["sort"]["ignorearticle"] = ignorearticle
+        return self.__sendRequest("VideoLibrary.GetMovies",params)
+
+    def GetMovieLibrarySize(self):
+        return len(self.__sendRequest("VideoLibrary.GetMovies")["movies"])
+    
+
+#### MISCELLANEOUS METHODS #############################################
+        
+    def Ping(self):
+        if self.__sendRequest("JSONRPC.Ping") == "pong":
+            return True
+        else:
+            return False
+
     def GetURL(self, path):
         params = {}
         params["path"] = path
         try:
-            return self.__sendRequest("Files.PrepareDownload",params)["details"]["path"]
+            return self.__sendRequest("Files.PrepareDownload",
+                                       params)["details"]["path"]
         except:
             return None
-
-
-
